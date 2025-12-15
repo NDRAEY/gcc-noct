f4a6d4bfa9d1c0bc7c67a991e3921b4400373991
Andrey <pikachu_andrey@vk.com>
2025-12-16 06:25:51 +0900

    Add NocturneOS support

--- fixincludes/mkfixinc.sh
+++ fixincludes/mkfixinc.sh
@@ -19,6 +19,7 @@ case $machine in
     powerpcle-*-eabisim* | \
     powerpcle-*-eabi* | \
     *-*-vxworks7* | \
+    *-nocturne* | \
     *-musl* )
 	#  IF there is no include fixing,
 	#  THEN create a no-op fixer and exit
